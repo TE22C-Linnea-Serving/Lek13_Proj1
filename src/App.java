ccbd6fdd353985ff17eb224d1cd02b049e651330
Linnea Serving <linnea.serving@elev..ga.ntig.se>
2024-02-05 09:15:05 +0100

    Användaren kan försvara, och f1 fungerar

--- src/App.java
+++ src/App.java
@@ -10,35 +10,19 @@ public class App {
         int attack1 = 0;
         int attack2 = 0;            
         int f1=1;
+        int f2=1;
         int HP1 = 150;
         int HP2 = 150;
-        
-        int m=0;
-        while(m==0){
-            
-            
 
-                    
+        while(HP1>0 && HP2>0){
+               
             //Meny:
-            
-            if(HP1<=0 && HP1<HP2){
-                System.out.println("Du Förlorade!");
-                m=1;
-                
-
-            }else if(HP2<=0 && HP2<HP1){
-                System.out.println("Du Vann!");
-                m=1;
-                
-
-            }else if(HP1==HP2 && HP1<=0){
-                System.out.println("Det blev lika!");
-                m=1;
-            }
             
             System.out.println("\nMeny:\n1. Attack\n2. Försvar\n3. Info");          
             System.out.println("\nDu har "+HP1+" HP\nDin motståndare har "+HP2+" HP");
             int svar = tangentbord.nextInt();
+
+            if(f1==1){
 
             //Användaren
             if(svar==1){                                            //Attack
@@ -54,6 +38,15 @@ public class App {
                 }
 
             }else if(svar==2){      //Användaren försvarar
+                int missAF = tärning.nextInt(2);
+                if(missAF==0){      //Misslyckas med försvar
+                    attack1=0;
+                    System.out.println("Försvaret misslyckades!");
+                }else{              //Lyckas med försvar
+                    System.out.println("Försvaret lyckades!");
+                    attack2/=2;
+                    f2=0;
+                }
 
             }else if(svar==3){      //Info
                 System.out.println("\n1. Du börjar med 150 HP. Ditt mål är att få din motståndares HP till 0.\n2. Attack har en 90% chans att lyckas. Om lyckad gör du 1-20 skada på motståndaren.\n3. Försvar har en 50% chans att lyckas. Om lyckad, halverar du motståndarens attack, \noch gör att dom inte kan röra sig nästa runda.\n");
@@ -62,8 +55,12 @@ public class App {
             }else{
                 System.out.println("Du skrev in ett ogitligt tal. Försök igen.");
                 System.out.println();
-                m=0;
             }
+
+        }else{
+            System.out.println("Du kan inte röra dig denna runda");
+            f1=1;
+        }
             
             //Motståndaren:
             int val1 = tärning.nextInt(6);                //Försvar eller attack
@@ -75,15 +72,15 @@ public class App {
                 if(missMf==0){                                  //Försvar misslyckas
                     System.out.println("försvaret misslyckades!");
                     HP2-=attack1;
-                    m=0;
                 
                 }else{                                          //Försvar lyckas
                     System.out.println("försvaret lyckades!");
-                    f1 = 0;
                     attack1 /= 2; 
                     System.out.println("Du gjorde totalt "+attack1+" skada!");
                     HP2-=attack1;
-                    m=0;
+                    if(svar==1){
+                        f1 = 0;
+                    }
                 }
             
             }else{                                                              //Attack
@@ -92,18 +89,27 @@ public class App {
                     attack2 = 0;
                     System.out.println("Din motståndare missar sin attack!");
                     HP2-=attack1;
-                    m=0;
                 
                 }else{                                                          //Attacken lyckas
                     attack2 = tärning.nextInt(20);
                     System.out.println("Din motståndare gör "+attack2+" sakda!");
                     HP2-=attack1;
                     HP1-=attack2;
-                    m=0;
                 }
             }   
 
-                } 
-                System.out.println("GAME OVER");
-            }
+        }
+
+                
+
+                    if(HP1<=0 && HP1<HP2){
+                        System.out.println("Du Förlorade!");
+                        
+                    }else if(HP2<=0 && HP2<HP1){
+                        System.out.println("Du Vann!");
+        
+                    }else if(HP1==HP2 && HP1<=0){
+                        System.out.println("Det blev lika!");
+                    }
+                }
             }
