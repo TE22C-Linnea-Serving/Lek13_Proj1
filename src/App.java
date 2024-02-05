158dc5e591fe6d386885511842f5fe3014e7e2bc
Linnea Serving <linnea.serving@elev..ga.ntig.se>
2024-02-05 08:42:47 +0100

    Fixade programmet

--- src/App.java
+++ src/App.java
@@ -2,42 +2,44 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {       //Att göra:
+                                                                    //While loopen fungerar inte (Fixa)
+
         Scanner tangentbord = new Scanner(System.in);
         Random tärning = new Random();
         int attack1 = 0;
-        int attack2 = 0;
+        int attack2 = 0;            
         int f1=1;
         int HP1 = 150;
         int HP2 = 150;
-
-        if(f1==1){
+        
         int m=0;
-        while(m==0){            //Meny:
-                        
+        while(m==0){
+            
+            
 
-            if(HP1<=0||HP1<=0&&HP2<=0&&HP1<HP2){
-                m=1;
+                    
+            //Meny:
+            
+            if(HP1<=0 && HP1<HP2){
                 System.out.println("Du Förlorade!");
-
-            }else if(HP2<=0||HP2<=0&&HP1<=0&&HP2<HP1){
                 m=1;
+                
+
+            }else if(HP2<=0 && HP2<HP1){
                 System.out.println("Du Vann!");
-
-            }else if(HP1==HP2){
                 m=1;
+                
+
+            }else if(HP1==HP2 && HP1<=0){
                 System.out.println("Det blev lika!");
-            }else{
-                break;
+                m=1;
             }
-
-
-             
-
+            
             System.out.println("\nMeny:\n1. Attack\n2. Försvar\n3. Info");          
             System.out.println("\nDu har "+HP1+" HP\nDin motståndare har "+HP2+" HP");
             int svar = tangentbord.nextInt();
-            
+
             //Användaren
             if(svar==1){                                            //Attack
                 int miss1 = tärning.nextInt(10);              //Ser om miss
@@ -51,43 +53,6 @@ public class App {
                     System.out.println("\nDu gör "+attack1+" skada!");
                 }
 
-
-
-
-                //Motståndaren:
-                int val1 = tärning.nextInt(6);                //Försvar eller attack
-                if(val1==0){                                        //Försvar
-
-                    System.out.println("\nMotståndaren försvarar...");
-                    int missMf = tärning.nextInt(2);
-
-                    if(missMf==0){                                  //Försvar misslyckas
-                        System.out.println("försvaret misslyckades!");
-                        HP2-=attack1;
-
-                    }else{                                          //Försvar lyckas
-                        System.out.println("försvaret lyckades!");
-                        f1 = 0;
-                        attack1 /= 2; 
-                        System.out.println("Du gjorde totalt "+attack1+" skada!");
-                        HP2-=attack1;
-                    }
-                    
-                }else{                                                              //Attack
-
-                    int missMa = tärning.nextInt(9);
-                    if(missMa==0){                                                  //Attacken missar
-                        attack2 = 0;
-                        System.out.println("Din motståndare missar sin attack!");
-                        HP2-=attack1;
-                    }else{                                                          //Attacken lyckas
-                        attack2 = tärning.nextInt(20);
-                        System.out.println("Din motståndare gör "+attack2+" sakda!");
-                        HP2-=attack1;
-                        HP1-=attack2;
-                    }
-                }
-
             }else if(svar==2){      //Användaren försvarar
 
             }else if(svar==3){      //Info
@@ -98,14 +63,47 @@ public class App {
                 System.out.println("Du skrev in ett ogitligt tal. Försök igen.");
                 System.out.println();
                 m=0;
-
             }
+            
+            //Motståndaren:
+            int val1 = tärning.nextInt(6);                //Försvar eller attack
+            if(val1==0){                                        //Försvar
+                
+                System.out.println("\nMotståndaren försvarar...");
+                int missMf = tärning.nextInt(2);
+                
+                if(missMf==0){                                  //Försvar misslyckas
+                    System.out.println("försvaret misslyckades!");
+                    HP2-=attack1;
+                    m=0;
+                
+                }else{                                          //Försvar lyckas
+                    System.out.println("försvaret lyckades!");
+                    f1 = 0;
+                    attack1 /= 2; 
+                    System.out.println("Du gjorde totalt "+attack1+" skada!");
+                    HP2-=attack1;
+                    m=0;
+                }
+            
+            }else{                                                              //Attack
+                int missMa = tärning.nextInt(9);
+                if(missMa==0){                                                  //Attacken missar
+                    attack2 = 0;
+                    System.out.println("Din motståndare missar sin attack!");
+                    HP2-=attack1;
+                    m=0;
+                
+                }else{                                                          //Attacken lyckas
+                    attack2 = tärning.nextInt(20);
+                    System.out.println("Din motståndare gör "+attack2+" sakda!");
+                    HP2-=attack1;
+                    HP1-=attack2;
+                    m=0;
+                }
+            }   
 
-
-        }
-    }else{
-        System.out.println("Du kan inte röra dig denna runda.");
-    }
-        
-    }
-}
+                } 
+                System.out.println("GAME OVER");
+            }
+            }
